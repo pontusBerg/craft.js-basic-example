4383be7e8ea623763c75ea3a79de1c4d611de336
pontusBerg <49114004+pontusBerg@users.noreply.github.com>
2023-12-21 15:26:46 +0100

    Chore: Migrate Toolbox.js from Material UI

--- src/components/Toolbox.js
+++ src/components/Toolbox.js
@@ -1,10 +1,4 @@
 import { useEditor, Element } from '@craftjs/core';
-import {
-  Box,
-  Typography,
-  Grid,
-  Button as MaterialButton,
-} from '@material-ui/core';
 import React from 'react';
 
 import { Button } from './user/Button';
@@ -19,31 +13,31 @@ export const Toolbox = () => {
     <div px={2} py={2}>
       <div
       >
-        <Box pb={2}>
-          <Typography>Drag to add</Typography>
-        </Box>
-        <Grid container direction="column" item>
-          <MaterialButton
+        <div pb={2}>
+          <div>Drag to add</div>
+        </div>
+        <div container direction="column" item>
+          <button
             ref={(ref) =>
-              connectors.create(ref, <Button text="Click me" size="small" />)
+              connectors.create(ref, <button>Click me</button>)
             }
             variant="contained"
             data-cy="toolbox-button"
           >
             Button
-          </MaterialButton>
-        </Grid>
-        <Grid container direction="column" item>
-          <MaterialButton
+          </button>
+        </div>
+        <div container direction="column" item>
+          <button
             ref={(ref) => connectors.create(ref, <Text text="Hi world" />)}
             variant="contained"
             data-cy="toolbox-text"
           >
             Text
-          </MaterialButton>
-        </Grid>
-        <Grid container direction="column" item>
-          <MaterialButton
+          </button>
+        </div>
+        <div container direction="column" item>
+          <button
             ref={(ref) =>
               connectors.create(
                 ref,
@@ -54,17 +48,17 @@ export const Toolbox = () => {
             data-cy="toolbox-container"
           >
             Container
-          </MaterialButton>
-        </Grid>
-        <Grid container direction="column" item>
-          <MaterialButton
+          </button>
+        </div>
+        <div container direction="column" item>
+          <button
             ref={(ref) => connectors.create(ref, <Card />)}
             variant="contained"
             data-cy="toolbox-card"
           >
             Card
-          </MaterialButton>
-        </Grid>
+          </button>
+        </div>
       </div>
     </div>
   );
